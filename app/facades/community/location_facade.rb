a75ca4c2e66a97accf7700a04b17bf7750ec2fb8
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-12-21 15:40:16 -0700

    Facade updates

--- app/facades/community/location_facade.rb
+++ app/facades/community/location_facade.rb
@@ -1,9 +1,15 @@
-class LocationFacade 
-  # require 'pry'; binding.pry
-  service = LocationService.new
-  request = service.location_search(zip)
-  data = request[:results].first[:address_components]
-  communities = data.map do |c|
-    Community.new(c)
+class LocationFacade
+  def initialize(input)
+    @input = input
+  end
+
+  def process_location_search
+    service = LocationService.new
+    request = service.location_search(@input)
+    data = request[:results].first[:address_components]
+    communities = data.map do |c|
+      Community.new(c)
+    end
+    communities
   end
 end
