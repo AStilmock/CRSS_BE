8eae593250e542bc9ba86388dc75d721c59b3529
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-01-17 16:04:39 -0700

    tests updated for live request and response, all tests currently passing.

--- spec/requests/location_request_spec.rb
+++ spec/requests/location_request_spec.rb
@@ -2,8 +2,64 @@ require 'rails_helper'
 
 RSpec.describe 'Location Request API' do
   describe 'mutation request' do
-    xit 'Not sure yet' do
+    it 'gets successfull response' do
+      zip_code = "80226"
+      mutation = <<~GQL
+        mutation{findCommunity(zipCode: "#{zip_code}") {
+          name
+          county
+          state
+          zipCode
+          }
+        }
+      GQL
 
+      post '/graphql', params: { query: mutation }
+      expect(status).to eq 200
+      expect(response).to be_successful
+    end
+    
+    it 'gets data back in successful response' do
+      zip_code = "80226"
+      mutation = <<~GQL
+        mutation{findCommunity(zipCode: "#{zip_code}") {
+          name
+          county
+          state
+          zipCode
+        }
+      }
+      GQL
+    
+      post '/graphql', params: { query: mutation }
+      object = JSON.parse(response.body, symbolize_names: true)
+      community = object[:data][:findCommunity]
+      expect(community).to be_a Hash
+      expect(community).to have_key(:name)
+      expect(community).to have_key(:county)
+      expect(community).to have_key(:state)
+      expect(community).to have_key(:zipCode)
+    end
+
+    it 'gets values from the keys in the response' do
+      zip_code = "80226"
+      mutation = <<~GQL
+        mutation{findCommunity(zipCode: "#{zip_code}") {
+          name
+          county
+          state
+          zipCode
+        }
+      }
+      GQL
+    
+      post '/graphql', params: { query: mutation }
+      object = JSON.parse(response.body, symbolize_names: true)
+      community = object[:data][:findCommunity]
+      expect(community[:name]).to eq "Lakewood"
+      expect(community[:county]).to eq "Jefferson County"
+      expect(community[:state]).to eq "CO"
+      expect(community[:zipCode]).to eq zip_code
     end
   end
 end
