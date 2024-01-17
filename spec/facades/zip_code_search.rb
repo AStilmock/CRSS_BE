bc23bac1d95a9d6fb6fd16f9ca86ba9a5ce24751
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-01-17 15:22:36 -0700

    completed facade testing with zip code query, all tests passing

--- spec/facades/zip_code_search.rb
+++ spec/facades/zip_code_search.rb
@@ -1,5 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Zip Code Facade Creation' do
-#   describe 'Facade'
-# end
+RSpec.describe 'Zip Code Facade Creation' do
+  describe 'Facade creation with zip code input' do
+    it 'returns community object' do
+      zip_code = '80226'
+      community = LocationFacade.new(zip_code).process_location_search
+      expect(community).to be_a CommunityPoro
+      expect(community.city).to eq 'Lakewood'
+      expect(community.county).to eq 'Jefferson County'
+      expect(community.state).to eq 'CO'
+      expect(community.zip_code).to eq zip_code
+    end
+  end
+end
