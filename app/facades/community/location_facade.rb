9f328d61c21c2cccff5f57a27cdc8e04f45e570a
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-01-17 14:17:32 -0700

    new schema updates for facade

--- app/facades/community/location_facade.rb
+++ app/facades/community/location_facade.rb
@@ -1,9 +1,11 @@
 class LocationFacade
   def initialize(input)
+    # require 'pry'; binding.pry
     @input = input
   end
 
   def process_location_search
+    # require 'pry'; binding.pry
     service = LocationService.new
     request = service.location_search(@input)
     data = request[:results].first[:address_components]
