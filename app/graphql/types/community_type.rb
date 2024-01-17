10ffc8aa0db454640548c9ace8f7c0479e0ba43d
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-01-17 16:04:04 -0700

    updates to types files for database object attribute type update

--- app/graphql/types/community_type.rb
+++ app/graphql/types/community_type.rb
@@ -4,7 +4,7 @@ module Types
     field :name, String
     field :county, String
     field :state, String
-    field :zip_code, Integer
+    field :zip_code, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,15 +1,10 @@
 module Types
   class MutationType < Types::BaseObject
-    # field :findCommunity, mutation: Mutations::FindCommunity do
-    #   argument :zipCode, GraphQL::Types::String, required: true
-    # end
-
-    field :findCommunity, Types::CommunityType, null: false do
+    field :find_community, Types::CommunityType, null: false do
       argument :zip_code, String, required: true
     end
 
     def find_community(zip_code:)
-      require 'pry'; binding.pry
       community = LocationFacade.new(zip_code).process_location_search
     end
   end
