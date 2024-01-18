c78995630bde2c906bd51b975ff26108d4a55304
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-01-18 16:06:09 -0700

    added endpoints from census data api with new service

--- app/services/census_data_service.rb
+++ app/services/census_data_service.rb
@@ -13,4 +13,20 @@ class CommunityResilienceService
   def resilience_data_search
     get_url("2021/cre")
   end
+
+  def economic_census
+    get_url("2017/ecnbasic")
+  end
+
+  def population_estimates
+    get_url("2021/pep/population")
+  end
+
+  def public_sector
+    get_url("timeseries/govs")
+  end
+
+  def zip_code_business_trends
+    get_url("2018/zbp")
+  end
 end
