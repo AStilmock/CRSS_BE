e4a4de2175e414b0f43058a8211ff55ed19f5629
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-01-20 08:22:21 -0700

    service and test file updates for air quality api troubleshooting

--- app/services/air_quality_service.rb
+++ app/services/air_quality_service.rb
@@ -1,17 +1,17 @@
 class AirQualityService
   def conn
     Faraday.new(url: "https://aqs.epa.gov/data/api/") do |f|
-      f.params["email"] = ENV["ANDYEMAIL"]
-      f.params["key"] = ENV["airqualiski"]
+      
     end
   end
 
   def get_url(url)
     response = conn.get(url)
+    require 'pry'; binding.pry
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def test_endpoint
-    get_url("metaData/revisionHistory")
+    get_url("metaData/revisionHistory?email=#{ENV["andymailski"]}&key=#{ENV["airqualiski"]}")
   end
 end
--- spec/services/air_quality_service_spec.rb
+++ spec/services/air_quality_service_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Air Quality API Service' do
   describe 'air quality api request' do
     it 'gets a successful response' do
-      state_code = "08"
-      county_code = "059"
+      # state_code = "08"
+      # county_code = "059"
       data = AirQualityService.new.test_endpoint
       parsed_data = JSON.parse(response.body, symbolize_names: true)
       require 'pry'; binding.pry
